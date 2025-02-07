9e3884a4d41071114480107d73f7d01e5534515c
Jane Zdravevski <jane@zdravevski.pro>
2025-02-07 09:29:19 +0100

    added a list of pre-configured data as 'birthday people' and set a function to clear them all using useState hook

---
+++ birthday-buddy/README.md
@@ -0,0 +1,3 @@
+# Birthday Buddy
+
+In this example project, I am just trying to learn how the useState hook works, and how to clear a list of data array, add new ones, etc
--- birthday-buddy/src/App.jsx
+++ birthday-buddy/src/App.jsx
@@ -1,4 +1,29 @@
+import { useState } from "react";
+import data from "./data";
+
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
+    const [people, setPeople] = useState(data);
+    const removeAll = () => {
+        setPeople([]);
+    };
+    return (
+        <>
+            <h2>Birthday Reminder - Starter</h2>
+            <div className="birthday-list">
+                {people.map((person) => {
+                    return (
+                        <div className="birthday" key={person.id}>
+                            <img src={person.image} />
+                            <h3>{person.name}</h3>
+                            <p>{person.age}</p>
+                        </div>
+                    );
+                })}
+                <button type="button" onClick={removeAll}>
+                    Remove All
+                </button>
+            </div>
+        </>
+    );
 };
 export default App;
