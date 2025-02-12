1a4ad9a7d7bae2838f6e0b3c06700f1d9881bafe
Jane Zdravevski <jane@zdravevski.pro>
2025-02-12 23:28:02 +0100

    removed the comments for the other ways of doing this, to make the code a bit more readable...

--- reviews/src/App.jsx
+++ reviews/src/App.jsx
@@ -6,39 +6,6 @@ const App = () => {
     const [index, setIndex] = useState(0);
     const { name, job, image, text } = people[index];
 
-    // const movePerson = (direction) => {
-    //     if (direction === 1) {
-    //         setIndex((currentIndex) => {
-    //             if (currentIndex === people.length - 1) {
-    //                 return 0;
-    //             } else {
-    //                 return currentIndex + 1;
-    //             }
-    //         });
-    //     } else {
-    //         setIndex((currentIndex) => {
-    //             if (currentIndex === 0) {
-    //                 return people.length - 1;
-    //             } else {
-    //                 return currentIndex - 1;
-    //             }
-    //         });
-    //     }
-    // };
-    // you can do it with the commented approach above, and also the buttons below.
-    // or you can do it with this aproach below. The result is same... it's just a matter of preference.
-
-    // const nextPerson = () => {
-    //     if (index === people.length - 1) {
-    //         setIndex(0);
-    //     } else {
-    //         setIndex(index + 1);
-    //     }
-    // }
-    //
-    // you can also use this commented approach, without using the setIndex(() => {})
-    // function approach, which is more readable, but using the other approach, takes the latest state value
-
     const nextPerson = () => {
         setIndex((currentIndex) => {
             if (currentIndex === people.length - 1) {
@@ -73,12 +40,6 @@ const App = () => {
                 <p className="job">{job}</p>
                 <p className="info">{text}</p>
                 <div className="btn-container">
-                    {/* <button className="prev-btn" onClick={() => movePerson(-1)}> */}
-                    {/*     <FaChevronLeft /> */}
-                    {/* </button> */}
-                    {/* <button className="next-btn" onClick={() => movePerson(1)}> */}
-                    {/*     <FaChevronRight /> */}
-                    {/* </button> */}
                     <button className="prev-btn" onClick={prevPerson}>
                         <FaChevronLeft />
                     </button>
