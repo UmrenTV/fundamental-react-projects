837e7832f339d62e723211fc33dc34291329cd39
Jane Zdravevski <jane@zdravevski.pro>
2025-02-13 00:06:22 +0100

    added randomPerson function, I did checkNumber logic to check if number is valid, and I did couple other improvements - also this whole logic can be replaced with % (modulus operator) - just keep in mind

--- reviews/src/App.jsx
+++ reviews/src/App.jsx
@@ -6,27 +6,44 @@ const App = () => {
     const [index, setIndex] = useState(0);
     const { name, job, image, text } = people[index];
 
+    // this function evaluates whether next number is valid or not, and returns the number if it is valid, otherwise it returns 0 or the last number in the array
+    const checkNumber = (number) => {
+        if (number > people.length - 1) {
+            return 0;
+        }
+        if (number < 0) {
+            return people.length - 1;
+        }
+        return number;
+    };
+
     const nextPerson = () => {
         setIndex((currentIndex) => {
-            if (currentIndex === people.length - 1) {
-                return 0;
-            } else {
-                return currentIndex + 1;
-            }
+            const newIndex = currentIndex + 1;
+            return checkNumber(newIndex);
         });
     };
 
     const prevPerson = () => {
         setIndex((currentIndex) => {
-            if (currentIndex === 0) {
-                return people.length - 1;
-            } else {
-                return currentIndex - 1;
-            }
+            const newIndex = currentIndex - 1;
+            return checkNumber(newIndex);
         });
     };
 
-    console.log(people);
+    // this function will generate a random number between 0 and the length of the array, and if the number is the same as the current index, it will call itself again
+    const randomPerson = () => {
+        const randomNumber = Math.floor(Math.random() * people.length);
+        setIndex((currentIndex) => {
+            if (randomNumber === currentIndex) {
+                console.log("the same number");
+                if (people.length > 1) randomPerson();
+                // randomPerson(); // this will cause infinite loop if length is smaller than 1
+            }
+            return checkNumber(randomNumber);
+        });
+    };
+
     return (
         <main>
             <article className="review">
@@ -47,6 +64,9 @@ const App = () => {
                         <FaChevronRight />
                     </button>
                 </div>
+                <button className="btn btn-hipster" onClick={randomPerson}>
+                    Surprise Me
+                </button>
             </article>
         </main>
     );
