fe4b6df4fa877498b185514e0e2fc9e1e66a4fe3
Jane Zdravevski <jane@zdravevski.pro>
2025-02-19 04:22:10 +0100

    created basic nav nagvigation, active class + functionality, burger icon for toggling the menu, etc

--- navbar/src/App.jsx
+++ navbar/src/App.jsx
@@ -1,4 +1,41 @@
+import { useState, useEffect } from "react";
+import { links } from "./data";
+import { FaHamburger } from "react-icons/fa";
+
 const App = () => {
-  return <h2>Navbar Starter</h2>;
+    const [pathname, setPathname] = useState(window.location.pathname);
+    const [toggleMenu, setToggleMenu] = useState(true);
+    useEffect(() => {
+        setPathname(window.location.pathname);
+    });
+
+    console.log(links);
+    return (
+        <nav className="nav-main">
+            <div className="nav-toggle">
+                <FaHamburger
+                    className="hamburger"
+                    onClick={() => {
+                        setToggleMenu(!toggleMenu);
+                    }}
+                />
+            </div>
+            {toggleMenu && (
+                <div className="nav-container">
+                    {links.map(({ id, url, text }) => {
+                        return (
+                            <a
+                                href={url}
+                                key={id}
+                                className={`nav-link ${pathname === url ? "active" : ""}`}
+                            >
+                                {text}
+                            </a>
+                        );
+                    })}
+                </div>
+            )}
+        </nav>
+    );
 };
 export default App;
