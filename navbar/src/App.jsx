513744d3431de81c6ee24a6a5b32be2bab4ac0dc
Jane Zdravevski <jane@zdravevski.pro>
2025-02-25 09:20:19 +0100

    initial setup complete

--- navbar/src/App.jsx
+++ navbar/src/App.jsx
@@ -1,41 +1,6 @@
-import { useState, useEffect } from "react";
-import { links } from "./data";
-import { FaHamburger } from "react-icons/fa";
+import Navbar from "./Navbar";
 
 const App = () => {
-    const [pathname, setPathname] = useState(window.location.pathname);
-    const [toggleMenu, setToggleMenu] = useState(true);
-    useEffect(() => {
-        setPathname(window.location.pathname);
-    });
-
-    console.log(links);
-    return (
-        <nav className="nav-main">
-            <div className="nav-toggle">
-                <FaHamburger
-                    className="hamburger"
-                    onClick={() => {
-                        setToggleMenu(!toggleMenu);
-                    }}
-                />
-            </div>
-            {toggleMenu && (
-                <div className="nav-container">
-                    {links.map(({ id, url, text }) => {
-                        return (
-                            <a
-                                href={url}
-                                key={id}
-                                className={`nav-link ${pathname === url ? "active" : ""}`}
-                            >
-                                {text}
-                            </a>
-                        );
-                    })}
-                </div>
-            )}
-        </nav>
-    );
+    return <Navbar />;
 };
 export default App;
