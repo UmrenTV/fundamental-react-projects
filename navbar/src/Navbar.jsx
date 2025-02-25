e3698903b2b4c826565d26a8ac52cffdbecb6770
Jane Zdravevski <jane@zdravevski.pro>
2025-02-25 09:45:39 +0100

    made the toggle of links a bit more dynamic using getBoundingRect from JS, and that fixes if more links have to added

--- navbar/src/Navbar.jsx
+++ navbar/src/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { FaBars } from "react-icons/fa";
 import { links, social } from "./data";
 import logo from "./logo.svg";
@@ -6,8 +6,18 @@ import logo from "./logo.svg";
 const Navbar = () => {
     const [showLinks, setShowLinks] = useState(false);
     const toggleLinks = () => {
+        console.log();
         setShowLinks(!showLinks);
     };
+    const linksContainerRef = useRef(null);
+    const linksRef = useRef(null);
+
+    const linkStyles = {
+        height: showLinks
+            ? `${linksRef.current.getBoundingClientRect().height}px`
+            : "0px",
+    };
+
     return (
         <nav>
             <div className="nav-center">
@@ -18,13 +28,11 @@ const Navbar = () => {
                     </button>
                 </div>
                 <div
-                    className={
-                        showLinks
-                            ? "links-container show-container"
-                            : "links-container"
-                    }
+                    className="links-container"
+                    ref={linksContainerRef}
+                    style={linkStyles}
                 >
-                    <ul className="links">
+                    <ul className="links" ref={linksRef}>
                         {links.map((link) => {
                             const { id, url, text } = link;
                             return (
