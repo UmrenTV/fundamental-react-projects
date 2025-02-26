d9aea7baab2f2bdd8d8eaef48bac03ffb77e40ed
Jane Zdravevski <jane@zdravevski.pro>
2025-02-26 14:39:51 +0100

    created styles and animations for social links, sidebar links, close/open sidebar, etc - ONE way of doing it

--- sidebar/src/Sidebar.jsx
+++ sidebar/src/Sidebar.jsx
@@ -1,8 +1,10 @@
 import { useGlobalContext } from "./context";
 import { FaBars } from "react-icons/fa";
+import { links, social } from "./data";
 
 const Sidebar = () => {
     const { sidebar, setSidebar } = useGlobalContext();
+    console.log(links);
     return (
         <div className={`sidebar-container ${sidebar && "active"}`}>
             <div className="sidebar-header">
@@ -13,6 +15,39 @@ const Sidebar = () => {
                         setSidebar(!sidebar);
                     }}
                 />
+            </div>
+            <div className="sidebar-content">
+                <ul className="sidebar-links">
+                    {links.map(({ id, url, text, icon }) => {
+                        return (
+                            <li key={id} className={`sidebar-link`}>
+                                <a href={url}>
+                                    <span className="sidebar-link-icon">
+                                        {icon}
+                                    </span>
+                                    <span
+                                        className={`sidebar-link-text ${sidebar && "active"}`}
+                                    >
+                                        {text}
+                                    </span>
+                                </a>
+                            </li>
+                        );
+                    })}
+                </ul>
+                <ul className={`social-links ${sidebar && "active"}`}>
+                    {social.map(({ id, url, icon }) => {
+                        return (
+                            <li key={id} className="social-link">
+                                <a href={url} target="_blank">
+                                    <span className="social-link-icon">
+                                        {icon}
+                                    </span>
+                                </a>
+                            </li>
+                        );
+                    })}
+                </ul>
             </div>
         </div>
     );
